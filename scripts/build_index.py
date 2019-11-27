7f4514bf55885229a43cd9ccbbb13473a08fb7ab
Emna Amor <Emna.amor8@gmail.com>
2019-11-27 14:13:39 +0100

    remove args

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -12,20 +12,16 @@ from tools.utils import my_collate
 
 
 class index(object):
-  def __init__(self, feature_list, model_path, index_path):
-    self.model_path = model_path
-    self.feature_list = feature_list
-    self.index_path = index_path
-    self.config = get_config(mode='train')
+  def __init__(self):
+    self.config = get_config()
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    self.weights = torch.load(self.model_path)
+    self.weights = torch.load(self.config.model_path)
     self.model = AutoLSTM(self.config.input_size, self.config.hidden_size,
                           self.config.num_layers, bidirectional=False)
     self.model.to(self.device)
     self.model.elstm.load_state_dict(self.weights)
     self.model.eval()
-    self.data_parser = DatasetParser(self.feature_list)
-
+    self.data_parser = DatasetParser(self.config.features_list)
     self.dl = DataLoader(self.data_parser, batch_size=self.config.batch_size,
                          shuffle=False,
                          num_workers=12, collate_fn=my_collate)
@@ -40,20 +36,10 @@ class index(object):
       outputs = outputs.detach().cpu().numpy()
       res = dict(zip(filepath, outputs))
       results.update(res)
-    with open(self.index_path) as fp:
+    with open(self.config.index_path) as fp:
       pickle.dump(results, fp)
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser('Build index for encoded video features')
-
-  parser.add_argument('-l', '--features_list', dest='features_list',
-                      help='Text file of video features list (.npy files)')
-  parser.add_argument('-d', '--destination', dest='destination',
-                      help='Location where to store the features index')
-  parser.add_argument('-m', '--model_path', dest='model_path',
-                      help='Lstm autoencoder model path')
-
-  args = parser.parse_args()
-  index = index(args.features_list, args.model_path, args.destination)
+  index = index()
   index.build_feature_index()
