37df0fef9ea19e0cf945271f220643932359b07e
Emna Amor <Emna.amor8@gmail.com>
2019-11-27 14:10:53 +0100

    feature: split video features by timestep=64

--- tools/utils.py
+++ tools/utils.py
@@ -1,6 +1,7 @@
 import logging
 import sys
-
+import numpy as np
+import random
 import yaml
 from torch.utils.data.dataloader import default_collate
 
@@ -51,3 +52,27 @@ def read_filelist(filepath):
 def my_collate(batch):
   batch = list(filter(lambda x: x is not None, batch))
   return default_collate(batch)
+
+def extract_video_features(filepath):
+  min_ts = 64
+  max_ts = 64
+  input_features_dim = 2048
+  f = np.load(filepath)
+  # sample a window from all timesteps
+  # of atleast self.min_ts and atmost self.max_ts
+  if f.shape[0] < min_ts:
+    return None
+
+  ts_begin = random.randint(0, f.shape[0] - min_ts)
+  ts_end = random.randint(ts_begin + min_ts,
+                          min(ts_begin + max_ts, f.shape[0]))
+
+  # get the features from that window
+  temp_win = np.zeros([max_ts, input_features_dim])
+  temp_win_rev = np.zeros_like(temp_win)
+  mask_rev = np.zeros([max_ts])
+  mask_rev[0: ts_end - ts_begin] = 1
+  temp_win[0: ts_end - ts_begin, :] = f[ts_begin:ts_end, :]
+  temp_win_rev[0: ts_end - ts_begin, :] = f[ts_begin:ts_end, :][::-1, :]
+
+  return filepath, temp_win, temp_win_rev, mask_rev
