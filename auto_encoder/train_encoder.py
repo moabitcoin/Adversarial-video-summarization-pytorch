be276064711d91359dcaf655c40a66c8c1c533ff
Emna Amor <Emna.amor8@gmail.com>
2019-11-27 15:40:18 +0100

    remove args

--- auto_encoder/train_encoder.py
+++ auto_encoder/train_encoder.py
@@ -10,39 +10,36 @@ from tqdm import tqdm
 from auto_encoder.lstm_network import AutoLSTM
 from configs.config import get_config
 from tools.dataset import DatasetParser
+from tools.tf_log_writer import LogWriter
 from tools.utils import my_collate
 
 
 class trainer(object):
   def __init__(self, config):
-    from tools.tf_log_writer import LogWriter
-    self.log_writer = LogWriter('/tmp/pycharm_project_753/logs')
-    self.config = config
+    self.config = get_config()
+    self.log_writer = LogWriter(self.config.log_dir)
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if torch.cuda.is_available():
       print("Using CUDA, benchmarking implementations", file=sys.stderr)
       torch.backends.cudnn.benchmark = True
+    self.model = AutoLSTM(config.input_size, config.hidden_size,
+                          config.num_layers, bidirectional=False)
+    self.model.to(self.device)
+    self.model = nn.DataParallel(self.model)
 
-  def train_model(self, model, num_epoches, model_save_dir, save_interval=1000):
-
+  def train_model(self):
     step = 0
     losses = []
-    num_workers = 12
     since = time.time()
-    learning_rate = 1e-4
-
-    model = model.to(self.device)
-    model = nn.DataParallel(model)
-
     criterion = nn.MSELoss()
     train_parser = DatasetParser(self.config.train_features_list)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
 
-    best_model_wts = copy.deepcopy(model.state_dict())
+    best_model_wts = copy.deepcopy(self.model.state_dict())
     dl = DataLoader(train_parser, batch_size=self.config.batch_size,
                     shuffle=True,
-                    num_workers=num_workers, collate_fn=my_collate)
-    for epoch in range(num_epoches):
+                    num_workers=self.config.num_workers, collate_fn=my_collate)
+    for epoch in range(self.config.n_epochs):
       train_loss = 0
       pbar = tqdm(dl, desc='Batch', ascii=True)
       try:
@@ -56,7 +53,7 @@ class trainer(object):
 
           optimizer.zero_grad()
 
-          outputs = model(inputs)
+          outputs = self.model(inputs)
 
           loss = criterion(outputs, inputs_rev)
           # loss = criterion(outputs, inputs)
@@ -66,30 +63,23 @@ class trainer(object):
 
           train_loss += loss.item() * inputs.size(1)
           print('loss {}'.format(loss.item()))
-          if step % save_interval == 0:
-            torch.save(model.module.elstm.state_dict(),
-                       '{}/encoder_lstm.pth-{}'.format(model_save_dir, step))
-            torch.save(model.module.state_dict(),
-                       '{}/autoencoder_encoder.pth-{}'.format(model_save_dir, step))
+          if step % self.config.save_interval == 0:
+            torch.save(self.model.module.state_dict(),
+                       '{}/autoencoder_encoder.pth-{}'.format(
+                         self.config.model_save_dir, step))
           au_loss = torch.stack(losses).mean()
           pbar.set_description('loss {}'.format(loss.item()))
           self.log_writer.update_loss(au_loss, epoch,
                                       'autoencoder loss')
           step += 1
-      except TypeError :
+      except TypeError:
         continue
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:0f}s'.format(time_elapsed // 60,
                                                        time_elapsed % 60))
 
-    model.load_state_dict(best_model_wts)
-    return model, losses
-
-
+    self.model.load_state_dict(best_model_wts)
+    return self.model, losses
 if __name__ == '__main__':
-  config = get_config(mode='train')
-  t = trainer(config)
-  model = AutoLSTM(config.input_size, config.hidden_size,
-                   config.num_layers, bidirectional=False)
-  model.to(t.device)
-  t.train_model(model, config.n_epochs, config.model_save_dir)
+  t = trainer()
+  t.train_model()
