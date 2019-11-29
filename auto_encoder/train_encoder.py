e7a5fa0ae00bd2ee2e108e366cc33135b3ebf8e4
Emna Amor <emna.amor8@gmail.com>
2019-11-29 17:09:06 +0100

    remove conf from init

--- auto_encoder/train_encoder.py
+++ auto_encoder/train_encoder.py
@@ -15,15 +15,15 @@ from tools.utils import my_collate
 
 
 class trainer(object):
-  def __init__(self, config):
+  def __init__(self):
     self.config = get_config()
     self.log_writer = LogWriter(self.config.log_dir)
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if torch.cuda.is_available():
       print("Using CUDA, benchmarking implementations", file=sys.stderr)
       torch.backends.cudnn.benchmark = True
-    self.model = AutoLSTM(config.input_size, config.hidden_size,
-                          config.num_layers, bidirectional=False)
+    self.model = AutoLSTM(self.config.input_size, self.config.hidden_size,
+                          self.config.num_layers, bidirectional=False)
     self.model.to(self.device)
     self.model = nn.DataParallel(self.model)
 
@@ -32,7 +32,7 @@ class trainer(object):
     losses = []
     since = time.time()
     criterion = nn.MSELoss()
-    train_parser = DatasetParser(self.config.train_features_list)
+    train_parser = DatasetParser(self.config.features_list)
     optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
 
     best_model_wts = copy.deepcopy(self.model.state_dict())
@@ -62,17 +62,19 @@ class trainer(object):
           losses.append(loss)
 
           train_loss += loss.item() * inputs.size(1)
-          print('loss {}'.format(loss.item()))
+          step += 1
+
+          pbar.set_description('loss {}'.format(loss.item()))
+
           if step % self.config.save_interval == 0:
             torch.save(self.model.module.state_dict(),
-                       '{}/autoencoder_encoder.pth-{}'.format(
+                       '{}/autoencoder_encoder-3l-{}.pth'.format(
                          self.config.model_save_dir, step))
-          au_loss = torch.stack(losses).mean()
-          pbar.set_description('loss {}'.format(loss.item()))
-          self.log_writer.update_loss(au_loss, epoch,
-                                      'autoencoder loss')
-          step += 1
-      except TypeError:
+        au_loss = torch.stack(losses).mean()
+        self.log_writer.update_loss(au_loss, epoch,
+                                    'autoencoder loss: False')
+      except Exception as e:
+        print(e)
         continue
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:0f}s'.format(time_elapsed // 60,
