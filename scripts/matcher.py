611360ab4587b24a5927c204ef729d297a2a3a0f
Emna Amor <emna.amor8@gmail.com>
2019-12-02 10:50:44 +0100

    get matched results: defaulto top 5

--- scripts/matcher.py
+++ scripts/matcher.py
@@ -1,11 +1,10 @@
-import argparse
 import pickle
 
 import numpy as np
 import scipy
 import torch
 from scipy import spatial
-
+import operator
 from auto_encoder.lstm_network import AutoLSTM
 from configs.config import get_config
 from tools.utils import extract_video_features
@@ -27,7 +26,7 @@ class Matcher(object):
     self.weights = torch.load(self.config.model_path)
     self.model = AutoLSTM(self.config.input_size, self.config.hidden_size,
                      self.config.num_layers, False)
-    self.model.elstm.load_state_dict(self.weights)
+    self.model.load_state_dict(self.weights)
     self.model.eval()
     self.model.to(self.device)
 
@@ -37,29 +36,31 @@ class Matcher(object):
     v = vector.reshape(1, -1)
     res = []
     for q in range(0, len(self.matrix)):
-      y = scipy.spatial.distance.cosine(self.matrix[q].reshape(1, -1), v)
+      y = scipy.spatial.distance.cdist(self.matrix[q].reshape(1,-1), v,
+                                       'cosine').reshape(-1)
       res.append(float(y))
     return res
 
   def match(self, features, topn=5):
     video_distances = self.cos_cdist(features)
     # getting top n videos
-    res = dict(sorted(zip(self.file_names, video_distances)))
-    firstnvals = {k: res[k] for k in sorted(res.keys())[:topn]}
-    nearest_video_paths, best_distances = zip(*firstnvals.items())
-    return nearest_video_paths, best_distances
+    res = dict(sorted(zip(video_distances, self.file_names)))
+    topn_results = {k: res[k] for k in list(res)[:topn]}
+    nearest_video_paths, best_distances = zip(*topn_results.items())
+    return best_distances, nearest_video_paths
 
   def search(self):
     d = extract_video_features(self.config.features_query)
     (filepath, inputs, inputs_rev, _) = d
     inputs = torch.from_numpy(inputs).float().unsqueeze(0).to(self.device)
-    outputs, (h_n, c_n) = self.model.elstm(inputs)
+    outputs = self.model.elstm(inputs)
     video_feat = outputs.cpu().detach().numpy()
-    video_paths, cosine_distances = matcher.match(video_feat, self.config.topn)
-    return video_paths, cosine_distances
+    paths, distances = matcher.match(video_feat, self.config.topn)
+    distances = [(1 - dist) for dist in distances]
+    return paths, distances
+
 
 if __name__ == '__main__':
   matcher = Matcher()
-  a, p = matcher.search()
-  print(a)
-  print(p)
+  path, distance = matcher.search()
+  print(dict(zip(path, distance)))
